5da6dbcfbe19079175493b616f3d0a6befc16622
Robert Strachan <r.m.strachan@gmail.com>
2017-11-24 14:03:55 -0700

    modal subscription lightbox completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,13 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
 var stickyHeader = new StickyHeader();
 
 var modalDescription = new ModalDescription();
+var modal = new Modal();
 
 
